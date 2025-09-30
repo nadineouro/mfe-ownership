9405e20ac943b30153d06a564628d6aadcb090e4
nadineouro <nadine.meyer.ouro@gmail.com>
2025-09-30 20:31:19 -0300

    chore: separate deliverybox and pickupbox (lib)

--- packages/shipping-components/src/index.ts
+++ packages/shipping-components/src/index.ts
@@ -1,14 +1,17 @@
 // Main exports
 export * from './icons';
 
-// Scenario 1 - Bug version
+// Scenario 1
 export { ShippingBox as Scenario1BugShippingBox } from './scenario-1/bug/ShippingBox';
-
-// Scenario 1 - Fixed version
 export { ShippingBox as Scenario1FixedShippingBox } from './scenario-1/fixed/ShippingBox';
 
 // Scenario 2
+export { ShippingBox as Scenario2InconsistentShippingBox } from './scenario-2/ShippingBoxOld';
 export { ShippingBox as Scenario2ShippingBox } from './scenario-2/ShippingBox';
 
 // Scenario 3
 export { ShippingBox as Scenario3ShippingBox } from './scenario-3/ShippingBox';
+
+// Scenario 4
+export { DeliveryBox as Scenario4DeliveryBox } from './scenario-4/DeliveryBox';
+export { PickupBox as Scenario4PickupBox } from './scenario-4/PickupBox';
