b33a8202d2406aaf51571b17c79cef345ad77262
nadineouro <nadine.meyer.ouro@gmail.com>
2025-09-30 19:39:35 -0300

    fix: avoid breakage when shipping price is not available

--- packages/shipping-components/src/index.ts
+++ packages/shipping-components/src/index.ts
@@ -2,4 +2,7 @@
 export * from './icons';
 
 // Scenario 1 - Bug version
-export { ShippingBox as ShippingBoxBug } from './scenario-1/bug/ShippingBox';
+export { ShippingBox as Scenario1BugShippingBox } from './scenario-1/bug/ShippingBox';
+
+// Scenario 1 - Fixed version
+export { ShippingBox as Scenario1FixedShippingBox } from './scenario-1/fixed/ShippingBox';
